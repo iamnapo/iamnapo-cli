8bdb3cdfbd479737a594d8250cbeffa19a0631a9
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-08-17 21:26:13 +0300

    Update eslint

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const React = require('react');
-const path = require('path');
-const meow = require('meow');
-const termImg = require('term-img');
-const importJsx = require('import-jsx');
-const { render } = require('ink');
+const React = require("react");
+const path = require("path");
+const meow = require("meow");
+const termImg = require("term-img");
+const importJsx = require("import-jsx");
+const { render } = require("ink");
 
-const ui = importJsx('./ui');
+const ui = importJsx("./ui");
 
 meow(`
   Usage
     $ iamnapo
 `);
 
-termImg(path.join(__dirname, 'avatar.png'), { fallback: () => {} });
+termImg(path.join(__dirname, "avatar.png"), { fallback: () => {} });
 render(React.createElement(ui));
--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,9 @@
-const React = require('react');
-const { Text, Box } = require('ink');
-const { default: SelectInput } = require('ink-select-input');
-const terminalLink = require('terminal-link');
-const open = require('open');
-const chalk = require('chalk');
+const React = require("react");
+const { Text, Box } = require("ink");
+const { default: SelectInput } = require("ink-select-input");
+const terminalLink = require("terminal-link");
+const open = require("open");
+const chalk = require("chalk");
 
 const handleSelect = (item) => {
   if (item.value) open(item.value);
@@ -11,22 +11,22 @@ const handleSelect = (item) => {
 };
 
 const items = [
-  { label: 'Website', value: 'https://iamnapo.me' },
-  { label: 'GitHub', value: 'https://github.com/iamnapo' },
-  { label: 'LinkedIn', value: 'https://linkedin.com/in/iamnapo' },
-  { label: 'Contact', value: 'mailto:napoleonoikon@gmail.com' },
-  { label: 'Facebook', value: 'https://facebook.com/napohotline' },
-  { label: 'Twitter', value: 'https://twitter.com/_iamnapo' },
-  { label: 'Quit', key: 'quit', action() { process.exit(); } },
+  { label: "Website", value: "https://iamnapo.me" },
+  { label: "GitHub", value: "https://github.com/iamnapo" },
+  { label: "LinkedIn", value: "https://linkedin.com/in/iamnapo" },
+  { label: "Contact", value: "mailto:napoleonoikon@gmail.com" },
+  { label: "Facebook", value: "https://facebook.com/napohotline" },
+  { label: "Twitter", value: "https://twitter.com/_iamnapo" },
+  { label: "Quit", key: "quit", action() { process.exit(); } },
 ];
 
 const ui = () => (
   <Box flexDirection="column">
-    <Text>{chalk.hex('#008B8B').bold('Hello! I’m Napoleon-Christos Oikonomou. 😎')}</Text>
+    <Text>{chalk.hex("#008B8B").bold("Hello! I’m Napoleon-Christos Oikonomou. 😎")}</Text>
     <Box marginBottom={1}>
       <Text>
         {`I’m an Electrical & Computer Engineer and Software Developer, currently working\nas a Software Engineer at the ${
-          terminalLink(chalk.cyan('Intelligent Systems & Software Engineering Labgroup'), 'https://issel.ee.auth.gr/')}.`}
+          terminalLink(chalk.cyan("Intelligent Systems & Software Engineering Labgroup"), "https://issel.ee.auth.gr/")}.`}
       </Text>
     </Box>
     <SelectInput items={items} onSelect={handleSelect} />
