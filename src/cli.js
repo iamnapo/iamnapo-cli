7cc195dfedf2e73cb6b2b082705b50a8c800eb86
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2020-04-28 15:12:26 +0300

    Remove image, Meta tweaks

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 const React = require("react");
-const path = require("path");
 const meow = require("meow");
-const termImg = require("term-img");
 const importJsx = require("import-jsx");
 const { render } = require("ink");
 
@@ -13,5 +11,4 @@ meow(`
     $ iamnapo
 `);
 
-termImg(path.join(__dirname, "avatar.png"), { fallback: () => { } });
 render(React.createElement(ui));
